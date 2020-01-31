d23d4270d75f35ad2fbe590fbec33883ad4eb1c4
yann300 <yann.levreau@gmail.com>
2020-01-31 22:47:36 +0100

    build profiles using circleci

---
+++ .circleci/build_profiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+SHA=`git rev-parse --short --verify HEAD`
+
+git config user.name "$COMMIT_AUTHOR"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git checkout origin/master
+
+echo "# Automatic build" > README.md
+# echo "Built website from \`$SHA\`. See https://github.com/ethereum/remix-ide/ for details." >> README.md
+# echo "To use an offline copy, download \`remix-$SHA.zip\`." >> README.md
+
+node ./dist/index.js
+
+git add build/profile_test.json
+git commit -m "Built profiles from {$SHA}."
+
+git push -f git@github.com:ethereum/remix-plugins-directory.git master
---
+++ .circleci/src/build_profiles.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs-extra';
+
+const buildProfiles = async () => {
+  const path = '../plugins'
+  fs.readdir(path, async (error, files) => {
+    if (error) return console.error(error)
+    console.log('profiles', files)
+    const profilesPromises = files.map(async (path) => {
+      const jsonProfile: any = await readFile(`../plugins/${path}/profile.json`)
+      return JSON.parse(jsonProfile)
+    })
+    const profiles = await Promise.all(profilesPromises)
+    console.log('built', JSON.stringify(profiles, null, '\t'))
+    const target = `../build/profile_test.json`
+    fs.writeFile(target, JSON.stringify(profiles, null, '\t'), 'utf8', (error) => { 
+      if (error) return console.error(error)
+      console.log('done', target)
+    })
+  })
+}
+
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      error ? reject(error) : resolve(data)
+    })  
+  })
+}
+
+buildProfiles()

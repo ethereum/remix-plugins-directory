96e17b9755bdf9b7b290d9d2b388bef6823723a0
yann300 <yann.levreau@gmail.com>
2020-02-02 11:05:25 +0100

    use more async

--- .circleci/src/build_profiles.ts
+++ .circleci/src/build_profiles.ts
@@ -1,23 +1,26 @@
 import * as fs from 'fs-extra';
 
-const buildProfiles = async () => {
-  const path = './plugins'
-  fs.readdir(path, async (error, files) => {
-    if (error) return console.error(error)
-    console.log('profiles', files)
-    const profilesPromises = files.map(async (path) => {
-      const jsonProfile: any = await readFile(`./plugins/${path}/profile.json`)
-      return JSON.parse(jsonProfile)
-    })
-    const profiles = await Promise.all(profilesPromises)
-    console.log('built', JSON.stringify(profiles, null, '\t'))
-
-    if (process.env.CIRCLE_BRANCH === 'master') {
-      const target = `./build/metadata.json`
-      fs.writeFile(target, JSON.stringify(profiles, null, '\t'), 'utf8', (error) => { 
-        if (error) return console.error(error)
-        console.log('done', target)
+const buildProfiles = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      const path = './plugins'
+      fs.readdir(path, async (error, files) => {
+        if (error) return reject(error)
+        console.log('profiles', files)
+        try {
+          const profilesPromises = files.map(async (path) => {
+            const jsonProfile: any = await readFile(`./plugins/${path}/profile.json`)
+            return JSON.parse(jsonProfile)
+          })
+          const profiles = await Promise.all(profilesPromises)
+          console.log('built', JSON.stringify(profiles, null, '\t'))
+          resolve(profiles) 
+        } catch (e) {
+          reject(e)
+        }             
       })
+    } catch (e) {
+      reject(e)
     }
   })
 }
@@ -31,4 +34,17 @@ const readFile = (path) => {
 }
 
 console.log('branch', process.env.CIRCLE_BRANCH)
-buildProfiles()
+console.log('pull request', process.env.CIRCLE_PULL_REQUEST)
+
+async function run () {
+  const profiles = await buildProfiles()
+  if (process.env.CIRCLE_BRANCH === 'master' && process.env.CIRCLE_PULL_REQUEST) {
+    const target = `./build/metadata.json`
+    fs.writeFile(target, JSON.stringify(profiles, null, '\t'), 'utf8', (error) => { 
+      if (error) return console.error(error)
+      console.log('done', target)
+    })    
+  }
+}
+
+run()

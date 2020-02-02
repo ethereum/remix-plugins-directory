f770ae690c19c0c8ab91844ca858776c2b07ade4
yann300 <yann.levreau@gmail.com>
2020-02-02 10:56:07 +0100

    remove commit to master

--- .circleci/build_profiles.sh
+++ .circleci/build_profiles.sh
@@ -9,8 +9,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 node ./.circleci/dist/index.js
 
-if [ "${CIRCLE_BRANCH}" == "master" ]; then
-    git add ./build/metadata.json
-    git commit -m "Built profiles from {$SHA}." --allow-empty
-    git push origin master
-fi
+# if [ "${CIRCLE_BRANCH}" == "master" ]; then
+#    git add ./build/metadata.json
+#    git commit -m "Built profiles from {$SHA}." --allow-empty
+#    git push origin master
+# fi

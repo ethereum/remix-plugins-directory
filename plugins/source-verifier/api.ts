f952f4eff0e5a3bc4dffe3a524c1858b2e334187
yann300 <yann.levreau@gmail.com>
2020-05-04 12:14:51 +0200

    add doc

--- plugins/source-verifier/api.ts
+++ plugins/source-verifier/api.ts
@@ -8,10 +8,20 @@ export interface VerifyResponse {
   message: string
 }
 
-// network should be either network name or network id; both number and string types are accepted
 export interface SourceVerifyApi {
   methods: {
+    /**
+     * Fetch contract metadata and source files.
+     * @param {string} address - contract address.
+     * @param {string | number} network - should be either network name or network id; both number and string types are accepted.
+     * @return {VerifiedArtifacts} - map containing a metadata.json and source files.
+     */
     fetch(address: string, network: string | number): VerifiedArtifacts
+    /**
+     * verify a contract. the network is retrieved from the application context.
+     * @param {verifyData} any - contract address.
+     * @return {VerifyResponse}
+     */
     verify(verifyData: any): VerifyResponse
   }
 }

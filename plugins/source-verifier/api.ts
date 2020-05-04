adfe795bdaa4ec4b30913cea8729d965d81edcc0
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-05-04 11:45:07 +0200

    Updating api

--- plugins/source-verifier/api.ts
+++ plugins/source-verifier/api.ts
@@ -3,9 +3,14 @@ export interface VerifiedArtifacts {
   contracts: any
 }
 
+export interface VerifyResponse {
+  success: boolean,
+  message: string
+}
+
 export interface SourceVerifyApi {
   methods: {
     fetch(address: string, network: string): VerifiedArtifacts
-    verify(verifyData: any): void //TODO: should this return success/error?
+    verify(verifyData: any): VerifyResponse
   }
 }

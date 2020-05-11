5e09761b842395f9797dc9d27edaaf8b1242ffe3
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-05-11 11:57:19 +0200

    Update api.ts

--- plugins/source-verifier/api.ts
+++ plugins/source-verifier/api.ts
@@ -18,17 +18,47 @@ export interface VerifyData {
 export interface SourceVerifyApi {
   methods: {
     /**
-     * Fetch contract metadata and source files.
+     * Fetch contract metadata and source files. Network is the one from Remix
+     * @param {string} address - contract address.
+     * @return {VerifiedArtifacts} - map containing a metadata.json and source files.
+     */
+    fetch(address: string): VerifiedArtifacts
+    
+    /**
+     * Fetch contract metadata and source files by providing custom network.
      * @param {string} address - contract address.
      * @param {string | number} network - should be either network name or network id; both number and string types are accepted.
      * @return {VerifiedArtifacts} - map containing a metadata.json and source files.
      */
-    fetch(address: string, network: string | number): VerifiedArtifacts
+    fetchByNetwork(address: string, chain: any): VerifiedArtifacts
+    
+     /**
+     * Fetch contract metadata and source files and save them to Remix's local storage.
+     * @param {string} address - contract address.
+     * @param {string | number} network - should be either network name or network id; both number and string types are accepted.
+     * @return {VerifiedArtifacts} - map containing a metadata.json and source files.
+     */
+    fetchAndSave(address: string, chain: any)
+    
     /**
      * verify a contract. the network is retrieved from the application context.
      * @param {verifyData} - map containing a metadata.json, source files, address and chain
      * @return {VerifyResponse}
      */
-    verify(verifyData: VerifyData): VerifyResult
+    verify(address: string, files: any): VerifyResult
+    
+     /**
+     * Verify a contract. the network is retrieved from the application context.
+     * @param {verifyData} - map containing a metadata.json, source files, address and chain
+     * @return {VerifyResponse}
+     */
+    verifyByNetwork(verifyData: VerifyData): VerifyResult
+    
+     /**
+     * Verify a contract, by the 
+     * @param {formData} - from containing a metadata.json, source files, address and chain
+     * @return {VerifyResponse}
+     */
+    verifyByForm(formData: any): VerifyResult
   }
 }
